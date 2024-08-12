5a84003475460409b8654ed40f95e31d749968d5
JuliusMatadamas <julius3877@gmail.com>
2024-08-12 18:50:29 -0500

    Inicio del proyecto

---
+++ src/main/java/com/jm/bsideapp/BsideAppApplication.java
@@ -0,0 +1,13 @@
+package com.jm.bsideapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsideAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BsideAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jm/bsideapp/BsideAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jm.bsideapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsideAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
